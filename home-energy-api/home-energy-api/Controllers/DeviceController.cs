f6b8ce4cf5f525314b95e0058ef9751bddfa89bb
Alexandre Cavalcante <ale.santos49@gmail.com>
2022-06-16 22:38:39 -0300

    Base da api backend

---
+++ home-energy-api/home-energy-api/Controllers/DeviceController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace home_energy_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeviceController : ControllerBase
+    {
+        private readonly ILogger<DeviceController> _logger;
+
+        public DeviceController(ILogger<DeviceController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: api/<DeviceController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<DeviceController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            _logger.LogInformation("Just Get Info");
+
+            return "value";
+        }
+
+        // POST api/<DeviceController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DeviceController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DeviceController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
