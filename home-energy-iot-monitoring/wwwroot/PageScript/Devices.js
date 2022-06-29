be2f3b5c17c29f10d31719f5e3ad384726af862a
Alexandre Cavalcante <ale.santos49@gmail.com>
2022-06-29 00:02:04 -0300

    Desenvolvimento da interface de simulação

--- home-energy-iot-monitoring/wwwroot/PageScript/Devices.js
+++ home-energy-iot-monitoring/wwwroot/PageScript/Devices.js
@@ -1,5 +1,28 @@
 ﻿class Devices {
     constructor() {
+        this.connection = new signalR.HubConnectionBuilder().withUrl("/devicehub").build();
+        this.DeviceHubConnection();
+        this.setBtnEvents();
+    }
 
+    DeviceHubConnection() {
+        $("#add-device").attr("disabled", true)
+        this.connection.on("ReceiveMessage", function (device, message) {
+            
+            $("#list-log").append(`<li>Dispostivo:${user} > ${message}</li>`);
+        })
+
+        this.connection.start().then(function () {
+            $("#add-device").attr("disabled", false)
+        }).catch(function (err) {
+            return console.log(err.toString());
+        })
+    }
+
+    setBtnEvents() {
+        var ThisClass = this;
+        $("#add-device").click(() => {
+            
+        })
     }
 1}
