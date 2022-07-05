88dd138ad71577f3a7690d8f878c90a737bea8c8
Luiz Diniz <luiz.gustavon@hotmail.com>
2022-07-05 13:17:54 -0300

    - Criação dos DbSets e inclusão da connection string no appsettings da API principal

--- home-energy-iot-entities/DataBaseContext.cs
+++ home-energy-iot-entities/DataBaseContext.cs
@@ -1,8 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using home_energy_iot_entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace home_energy_iot_entities
 {
     public class DataBaseContext : DbContext
     {
+        private IConfiguration _configuration;
+
+        public DataBaseContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<DeviceReport> DevicesReports { get; set; }
+        public DbSet<HouseBill> HousesBills { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserHouse> UsersHouses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration["ConnectionString"]);
+        }
     }
 }
