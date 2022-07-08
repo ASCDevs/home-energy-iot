1c0e4fd995f122601b79e4604e48220638274c1b
Alexandre Cavalcante <ale.santos49@gmail.com>
2022-07-07 23:38:01 -0300

    Adicionado listener para requisições socket além do signalr

--- home-energy-iot-monitoring/Program.cs
+++ home-energy-iot-monitoring/Program.cs
@@ -1,13 +1,13 @@
 using home_energy_iot_monitoring.Hubs;
 using Microsoft.AspNetCore.ResponseCompression;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllersWithViews();
-//builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
@@ -23,13 +23,32 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseResponseCompression();
-app.UseStaticFiles(); //Adicinado
+app.UseStaticFiles(); 
 app.UseRouting();
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
+
+app.UseWebSockets();
+app.Map("/consocket", async context =>
+{
+    if (!context.WebSockets.IsWebSocketRequest)
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+    else
+    {
+        Console.WriteLine("Cliente websokcet conectou: "+context.TraceIdentifier);
+        using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+        while (true)
+        {
+            await webSocket.SendAsync(
+                Encoding.ASCII.GetBytes($"Info -> {DateTime.Now}"),
+                WebSocketMessageType.Text,
+                true, CancellationToken.None);
+            await Task.Delay(1000);
+        }
+    }
+});
 
 
 //app.MapControllers();
---
+++ home-energy-iot-monitoring/Sockets/WebSocketController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace home_energy_iot_monitoring.Sockets
+{
+    public class WebSocketController : ControllerBase
+    {
+        [HttpGet("/ws")]
+        public async Task Get()
+        {
+            if (HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+                //await Echo(webSocket);
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            }
+        }
+    }
+}
