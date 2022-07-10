9fc06f984b3836d145e48b6cd4006ab8406c4520
Luiz Diniz <luiz.gustavon@hotmail.com>
2022-07-10 17:51:50 -0300

    - Implementação base da autenticação utilizando JWT

---
+++ home-energy-backend/home-energy-iot-api/Authentication/TokenService.cs
@@ -0,0 +1,37 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using home_energy_iot_entities.Entities;
+using Microsoft.IdentityModel.Tokens;
+
+namespace home_energy_api.Authentication
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+        private static JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(User user)
+        {   
+            var jwtKeyBytes = Encoding.ASCII.GetBytes(_configuration["SecretJwtKey"]);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new Claim[] {
+                    new Claim(ClaimTypes.Name, user.Username.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(15),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(jwtKeyBytes), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
--- home-energy-backend/home-energy-iot-api/Program.cs
+++ home-energy-backend/home-energy-iot-api/Program.cs
@@ -1,32 +1,53 @@
+using System.Text;
+using home_energy_api.Authentication;
 using home_energy_iot_core;
 using home_energy_iot_core.Helpers;
 using home_energy_iot_core.Helpers.Interfaces;
 using home_energy_iot_core.Interfaces;
 using home_energy_iot_entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataBaseContext>();
 
 builder.Services.AddTransient<IUserManager, UserManager>();
 builder.Services.AddTransient<IHasher, Hasher>();
+builder.Services.AddSingleton<ITokenService, TokenService>();
 
 builder.Logging.AddLog4Net();
 
+var key = Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("SecretJwtKey"));
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+    };
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
 
 app.UseHttpsRedirection();
 
