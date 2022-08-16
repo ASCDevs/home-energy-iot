9099663878b19dfabf9296f8e559a6cba4b03e8c
Augusto Calisto <augustoaquino007@gmail.com>
2022-08-16 15:35:32 -0300

    Adicionando arquivos arduino

---
+++ home-energy-arduino/page_authenticate.h
@@ -0,0 +1,12 @@
+const char *pageAuthenticate = R"raw(
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Autenticar Wi-Fi</title>
+</head>
+<body>
+    <h2>PowerMetrics - AUTENTICADO COM SUCESSO</h2><br>
+    <p>Remover SSID e Senha: <a href="/cleareeprom">ClearEEPROM</a></p>
+</body>
+</html>)raw";
