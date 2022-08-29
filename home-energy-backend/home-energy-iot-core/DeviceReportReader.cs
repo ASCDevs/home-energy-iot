10218c7f1bcda939fa97767ba99ccbe7896a87b5
Luiz Diniz <luiz.gustavon@hotmail.com>
2022-08-29 13:18:40 -0300

    - Base da implementação para leitura de reports

---
+++ home-energy-backend/home-energy-iot-core/DeviceReportReader.cs
@@ -0,0 +1,29 @@
+﻿using home_energy_iot_core.Interfaces;
+
+namespace home_energy_iot_core
+{
+    internal class DeviceReportReader : IDeviceReportReader
+    {
+        public decimal GetDeviceConsumptionTotalValueInReal(string deviceIdentificationCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetDeviceConsumptionValueBetweenInReal(string deviceIdentificationCode, DateTime initialDate,
+            DateTime finalDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetDeviceConsumptionTotalValueInWatts(string deviceIdentificationCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetDeviceConsumptionValueBetweenInWatts(string deviceIdentificationCode, DateTime initialDate,
+            DateTime finalDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ home-energy-backend/home-energy-iot-core/Interfaces/IDeviceReportReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home_energy_iot_core.Interfaces
+{
+    public interface IDeviceReportReader
+    {
+        decimal GetDeviceConsumptionTotalValueInReal(string deviceIdentificationCode);
+        decimal GetDeviceConsumptionValueBetweenInReal(string deviceIdentificationCode, DateTime initialDate, DateTime finalDate);
+        decimal GetDeviceConsumptionTotalValueInWatts(string deviceIdentificationCode);
+        decimal GetDeviceConsumptionValueBetweenInWatts(string deviceIdentificationCode, DateTime initialDate, DateTime finalDate);
+    }
+}
