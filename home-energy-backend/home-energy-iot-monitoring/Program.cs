c19f15820f202646425bb0df491dbe648a893213
Alexandre Cavalcante <ale.santos49@gmail.com>
2022-09-12 00:12:59 -0300

    Limpando configuração de dependencias

--- home-energy-backend/home-energy-iot-monitoring/Program.cs
+++ home-energy-backend/home-energy-iot-monitoring/Program.cs
@@ -9,8 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR().AddJsonProtocol(options =>
 {
     options.PayloadSerializerOptions.PropertyNamingPolicy = null;
@@ -25,12 +23,6 @@ builder.Services.AddResponseCompression(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseResponseCompression();
 app.UseStaticFiles();
