745b8827064b6153a90b633b7eba678b30ae7c5a
Augusto Calisto <augustoaquino007@gmail.com>
2022-09-14 14:03:59 -0300

    Acrescentando overlay no login

--- home-energy-frontend/src/views/Auth.vue
+++ home-energy-frontend/src/views/Auth.vue
@@ -1,5 +1,13 @@
 <template>
     <div class="container border mt-3">
+        <div class="overlay" id="overlay">
+			<div class="overlay-content">
+				<span class="spinner"></span>
+				
+          		<p class="mt-3"> Autenticando... </p>
+			</div>
+		</div>
+
         <form class="p-3">
             <div class="form-group">
                 <label for="username"> Login </label>
@@ -43,11 +51,17 @@
 
         methods: {
             auth() {
+                let overlay = document.getElementById("overlay");
+
+                overlay.style.display = "block";
+
                 this.$store.dispatch('login', this.loginModel)
-                    .then((response) => {
+                    .then(() => {
                         this.$router.push({name: 'about'});
                     })
                     .catch((error) => {
+                        overlay.style.display = "none";
+
                         if(error.request.status == 400) {
                             this.errorAuth = error.response.data;
                         }
@@ -58,4 +72,66 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+    p {
+	    color: white;
+    }
+            
+    .spinner {
+        width: 75px;
+        height: 75px;
+        display: inline-block;
+        border-width: 2px;
+        border-color: rgba(255, 255, 255, 0.05);
+        border-top-color: #fff;
+        animation: spin 1s infinite linear;
+        border-radius: 100%;
+        border-style: solid;
+    }
+
+    @keyframes spin {
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+                
+    .overlay {
+        height: 100%;
+        width: 100%;
+        display: none;
+        position: fixed;
+        z-index: 1;
+        top: 0;
+        left: 0;
+        background-color: rgb(0, 0, 0);
+        background-color: rgba(0, 0, 0, 0.9);
+    }
+
+    .overlay-content {
+        position: relative;
+        top: 25%;
+        width: 100%;
+        text-align: center;
+        margin-top: 30px;
+    }
+
+    .overlay a {
+        padding: 8px;
+        text-decoration: none;
+        font-size: 36px;
+        color: #818181;
+        display: block;
+        transition: 0.3s;
+    }
+
+    .overlay a:hover, .overlay a:focus {
+        color: #f1f1f1;
+    }
+
+    .overlay .closebtn {
+        position: absolute;
+        top: 20px;
+        right: 45px;
+        font-size: 60px;
+    }
+</style>
