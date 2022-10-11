27e4421aaeb41c7a8c357fe08afaee3b494e889d
Alexandre Cavalcante <ale.santos49@gmail.com>
2022-10-10 23:03:30 -0300

    Atualição no envio do report do device

--- home-energy-backend/home-energy-iot-monitoring/Domains/DeviceReport.cs
+++ home-energy-backend/home-energy-iot-monitoring/Domains/DeviceReport.cs
@@ -5,23 +5,13 @@
         public int Id { get; set; }
         public string IdentificationCode { get; set; }
         public DateTime ReportDate { get; set; }
-        public decimal WattsUsage { get; set; }
+        public string WattsUsage { get; set; }
 
         public DeviceReport(string energyVale, string deviceId)
         {
-            WattsUsage = DecimalConverter(energyVale);
+            WattsUsage = energyVale;
             IdentificationCode = deviceId;
             ReportDate = DateTime.Now;
-        }
-
-        private decimal DecimalConverter(string value)
-        {
-            if (value.Contains("."))
-            {
-                value = value.Replace(".", ",");
-            }
-
-            return Convert.ToDecimal(value);
         }
     }
 }
