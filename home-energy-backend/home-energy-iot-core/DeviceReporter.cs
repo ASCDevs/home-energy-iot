19929f4cb635a473393fa8e9000981fdb4714074
Luiz Diniz <luiz.gustavon@hotmail.com>
2022-10-10 22:13:08 -0300

    - atualização do script e remoção do cálculo que já está sendo feito antes do processamento

--- home-energy-backend/home-energy-iot-core/DeviceReporter.cs
+++ home-energy-backend/home-energy-iot-core/DeviceReporter.cs
@@ -27,8 +27,6 @@ namespace home_energy_iot_core
 
                 report.ReportDate = DateTime.UtcNow.AddHours(-3);
 
-                report.WattsUsage = WattsUsage(report.WattsUsage);
-
                 _deviceReporterRepository.Report(report);
 
                 _logger.LogInformation($"Report do Dispositivo com Código de identificação [{report.IdentificationCode}] adicionado com sucesso.");
@@ -38,13 +36,6 @@ namespace home_energy_iot_core
                 _logger.LogError(ex, $"Erro ao reportar o Dispositivo.");
                 throw;
             }
-        }
-
-        private decimal WattsUsage(decimal wattsUsage)
-        {
-            var toString = String.Format("{0:00.00}", wattsUsage);
-            var conversionResult = Convert.ToDecimal(toString);
-            return conversionResult;
         }
 
         private void ValidateDeviceReport(DeviceReport report)
